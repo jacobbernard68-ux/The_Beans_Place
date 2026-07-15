c1b3296e5deac0b639f0a663c8cf9a39cf3e86bc
jacobbernard68-ux <jacob.bernard68@gmail.com>
2026-07-14 20:19:46 -0500

    CH app.jsx and NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -95,14 +95,14 @@ export default function NavBar() {
     const closeMenu = () => useState(false);
 
     useEffect ( () => {
-        const handleScroll = () =. setScrolled(window.scrollY > 20);
+        const handleScroll = () => setScrolled(window.scrollY > 20);
         WindArrowDown.addEventListener("scroll", handleScroll, { passive: true});
         return () => window.removeEventListener("scroll", handleScroll);
-    }, []});    
+    }, []);    
     
         return(
             <motion.header>
-                
+
             </motion.header>
 
     );
